64dfddb89049a6cded27b4e214d7f82cbdafd8dc
Hugo Paré <71098494+Hugo-Pare@users.noreply.github.com>
2021-07-18 20:57:38 -0400

    Added navbar

---
+++ portfolio-calculator/src/components/navbar/MenuItems.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '../../App.js',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Info',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'My Portfolio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign in',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
---
+++ portfolio-calculator/src/components/navbar/Navbar.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import { Button } from '../Button';
+import { MenuItems } from './MenuItems'
+import './Navbar.css'
+
+class Navbar extends Component {
+    state = { clicked: false}
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+    render(){
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">Portfolio Calculator <i class="fas fa-dollar-sign"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <Button>Sign In</Button>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
