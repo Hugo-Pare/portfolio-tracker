8f738cdc1e03c610e81561f9070ea63af47cada3
Hugo Paré <71098494+Hugo-Pare@users.noreply.github.com>
2021-07-23 13:50:54 -0400

    Added ticker data + visual effects

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from yahoo_fin.stock_info import get_data
 import yfinance as yf
+import yahoo_fin.stock_info as si
 
 from datetime import date, datetime, timedelta
 import json
@@ -47,19 +48,40 @@ def get_stock_price_by_date():
         # interval: {“1d”, “1wk”, “1mo”}    
         all_data = get_data(ticket, start_date=timeInterval, end_date=today, index_as_date = False, interval=interval)
         company_name = yf.Ticker(ticket).info['longName']
+        quote_table = si.get_quote_table(ticket, dict_result=False)
+        print(quote_table)
         
         dates = []
         stockPrice = []
+        attribute = []
+        value = []
 
         for date in all_data["date"]:
             dates.append(date.strftime('%d/%m/%Y'))
+
         for close in all_data["adjclose"]:
             stockPrice.append(str(close))
 
         data = {
             "date": dates,
             "stockPrice": stockPrice,
-            "companyName": company_name
+            "companyName": company_name,
+            "weekRange": str(quote_table['value'][1]),
+            "prevClose": str(quote_table['value'][14]),
+            "open": str(quote_table['value'][12]),
+            "bid": str(quote_table['value'][5]),
+            "ask": str(quote_table['value'][2]),
+            "dayRange": str(quote_table['value'][6]),
+            "yTargetEst": str(quote_table['value'][0]),
+            "averageVolume": str(quote_table['value'][3]),
+            "volume": str(quote_table['value'][16]),
+            "marketCap": str(quote_table['value'][11]),
+            "eps": str(quote_table['value'][7]),
+            "forwardDividend": str(quote_table['value'][10]),
+            "peRatio": str(quote_table['value'][13]),
+            "beta": str(quote_table['value'][4]),
+            "earningsDate": str(quote_table['value'][8]),
+            "exDividendDate": str(quote_table['value'][9])
         }
 
         return json.dumps(data)
--- portfolio-calculator/src/components/LineChart.js
+++ portfolio-calculator/src/components/LineChart.js
@@ -17,7 +17,17 @@ class LineChart extends Component {
             interval: '1wk',
             date: [],
             stockPrice: [],
-            companyName: ''
+            companyName: '',
+            validTicket: false,
+            weekRange: '',
+            prevClose: '',
+            open: '',
+            ask: '',
+            bid: '',
+            dayRange: '',
+            yTargetEst: '',
+            averageVolume: '',
+            volume: '', marketCap: '', eps: '', forwardDividend: '', peRatio: '', beta: '', earningsDate: '', exDividendDate: ''
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -27,10 +37,15 @@ class LineChart extends Component {
     async handleSubmit(e) {
         e.preventDefault();
 
-        await fetch(`http://127.0.0.1:5000/stockData?ticket=${this.state.ticket}&interval=${this.state.interval}`) // Include companyName with the same fetch call 
+        fetch(`http://127.0.0.1:5000/stockData?ticket=${this.state.ticket}&interval=${this.state.interval}`) // Include companyName with the same fetch call 
         .then(response => response.json())
         // .then(json => console.log(json))
-        .then(json => this.setState({date: json.date, stockPrice: json.stockPrice, companyName: json.companyName}))
+        .then(json => this.setState({
+            date: json.date, stockPrice: json.stockPrice, companyName: json.companyName, validTicket: true, attribute: json.attribute, value: json.value,
+            weekRange: json.weekRange, prevClose: json.prevClose, open: json.open, ask: json.ask, bid: json.bid, dayRange: json.dayRange, yTargetEst: json.yTargetEst,
+            averageVolume: json.averageVolume, volume: json.volume, marketCap: json.marketCap, eps: json.eps, forwardDividend: json.forwardDividend, peRatio: json.peRatio,
+            beta: json.beta, earningsDate: json.earningsDate, exDividendDate: json.exDividendDate
+        }))
     }
 
     handleSubmitInterval(e) {
@@ -49,30 +64,38 @@ class LineChart extends Component {
     }
 
     render() {
+
+        const validTicket = this.state.validTicket;
+
         return (
             <>
                 <div className="stock-finder">
                     <form onSubmit={(e) => {this.handleSubmit(e)}}>
-                        <label className="ticket">Stock ticket :</label>
+                        <label className="ticker">Stock ticker :</label>
                         <input className="input" onChange={this.handleChange}></input>
                         <Button type="submit" >
                             Find
                         </Button>
-                        {/* Add more interval buttons -> 1D, 1M, 3M, 6M, 10Y and remove 2M */}
-                        <div className="intervalButtons">
-                            <IntervalButton type="submit" onClick={(e) => this.handleInterval(e, "1d")}>2M</IntervalButton>
-                            <IntervalButton type="submit" onClick={(e) => this.handleInterval(e, "1wk")}>1Y</IntervalButton>
-                            <IntervalButton type="submit" onClick={(e) => this.handleInterval(e, "1mo")}>5Y</IntervalButton> 
-                        </div>
                         <div>
                             <h1 className="company-name">
                                 {this.state.companyName}
                             </h1>
                         </div>
+                        {/* Add more interval buttons -> 1D, 1M, 3M, 6M, 10Y and remove 2M */}
+                        {validTicket
+                        ?
+                            <div className="intervalButtons">
+                                <IntervalButton type="submit" onClick={(e) => this.handleInterval(e, "1d")}>2M</IntervalButton>
+                                <IntervalButton type="submit" onClick={(e) => this.handleInterval(e, "1wk")}>1Y</IntervalButton>
+                                <IntervalButton type="submit" onClick={(e) => this.handleInterval(e, "1mo")}>5Y</IntervalButton> 
+                            </div>
+                        : <div></div>
+                        }
                     </form>
                 </div>
                 
                 <div className="chart-container">
+                    {validTicket ?
                     <Line
                         data={{
                             labels: this.state.date,
@@ -97,7 +120,66 @@ class LineChart extends Component {
                                 }
                             }
                         }}
-                    />   
+                    /> 
+                    : <div></div>} 
+                </div>
+
+                <div className="stock-stats">
+                    {validTicket
+                        ? (
+                            <>
+                                <div className="row">
+                                    <div className="column">
+                                        <p><b>52 Week Range</b></p>
+                                        <p><b>Previous Close</b></p>
+                                        <p><b>Open</b></p>
+                                        <p><b>Ask</b></p>
+                                        <p><b>Bid</b></p>
+                                        <p><b>Day's Range</b></p>
+                                        <p><b>1y Target Est</b></p>
+                                        <p><b>Average Volume</b></p>
+                                        <p><b>Volume</b></p>
+                                    </div>
+                                        
+                                    <div className="column">
+                                        <p>{this.state.weekRange}</p>
+                                        <p>{this.state.prevClose}</p>
+                                        <p>{this.state.open}</p>
+                                        <p>{this.state.ask}</p>
+                                        <p>{this.state.bid}</p>
+                                        <p>{this.state.dayRange}</p>
+                                        <p>{this.state.yTargetEst}</p>
+                                        <p>{this.state.averageVolume}</p>
+                                        <p>{this.state.volume}</p>
+                                    </div>
+                                </div>
+
+                                <div className="row">
+                                    <div className="column">
+                                        <p><b>Market Cap</b></p>
+                                        <p><b>EPS (TTM)</b></p>
+                                        <p><b>Forward Dividend (Yield)</b></p>
+                                        <p><b>PE Ratio (TTM)</b></p>
+                                        <p><b>Beta (5Y Monthly)</b></p>
+                                        <p><b>Earnings Date</b></p>
+                                        <p><b>Ex-Dividend Date</b></p>
+                                    </div>
+                                        
+                                    <div className="column">
+                                        <p>{this.state.marketCap}</p>
+                                        <p>{this.state.eps}</p>
+                                        <p>{this.state.forwardDividend}</p>
+                                        <p>{this.state.peRatio}</p>
+                                        <p>{this.state.beta}</p>
+                                        <p>{this.state.earningsDate}</p>
+                                        <p>{this.state.exDividendDate}</p>
+                                    </div>
+                                </div>
+                            
+                             </>   
+                        )
+                        : <div></div> // Returns nothing
+                    }
                 </div>
             </>
         )
