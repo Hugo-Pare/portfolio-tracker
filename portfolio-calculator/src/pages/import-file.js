cc6dbdebf01fc8a1661ba2f8b103c116f193ebe6
Hugo Paré <71098494+Hugo-Pare@users.noreply.github.com>
2021-09-11 12:09:24 -0400

    Fixed import file, need style + change date format

--- portfolio-calculator/src/pages/import-file.js
+++ portfolio-calculator/src/pages/import-file.js
@@ -1,96 +1,96 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import * as XLSX from "xlsx";
+import XLSX from "xlsx";
 import './import-file.css'
 import { Button } from "../components/Button";
 import { Bar } from 'react-chartjs-2'
 import moment from 'moment'
+import MaterialTable from 'material-table';
+
+const EXTENSIONS = ['xlsx', 'xls', 'csv']
 
 function File() {
-    const [items, setItems] = useState([]);
-    // const titleList = Object.keys(items[1])
+    const [colDefs, setColDefs] = useState()
+    const [data, setData] = useState()
 
-    const readExcel = (file) => {
-        const promise = new Promise((resolve, reject) => {
-            const fileReader = new FileReader();
-            fileReader.readAsArrayBuffer(file);
-
-            fileReader.onload = (e) => {
-                const bufferArray = e.target.result;
-                const wb = XLSX.read(bufferArray, { type: "buffer" });
-
-                const wsname = wb.SheetNames[0];
-
-                const ws = wb.Sheets[wsname];
-
-                const data = XLSX.utils.sheet_to_json(ws);
-
-                resolve(data);
-            };
-
-            fileReader.onerror = (error) => {
-                reject(error);
-            };
-        });
-
-        promise.then((d) => {
-            setItems(d);
-            console.log(d);
-        });
-
-    };
-
-    const ExcelDateToJSDate = (date) => {
-        let converted_date = new Date(Math.round((date - 25568) * 864e5));
-        converted_date = String(converted_date).slice(4, 15)
-        date = converted_date.split(" ")
-        let day = date[1];
-        let month = date[0];
-        month = "JanFebMarAprMayJunJulAugSepOctNovDec".indexOf(month) / 3 + 1
-        if (month.toString().length <= 1)
-            month = '0' + month
-        let year = date[2];
-        return String(day + '-' + month + '-' + year)
+    const getExention = (file) => {
+        const parts = file.name.split('.')
+        const extension = parts[parts.length - 1]
+        return EXTENSIONS.includes(extension) // return boolean
     }
 
-    return (
-        <>
-            <div style={{
-                display:'flex',
-                justifyContent:'center',
-                alignItems:'center',
-            }}>
-                <h1>Portfolio Dashboard</h1>
-            </div>
-            <div className="input-file">
-                <label className="custome-file-upload">
-                    <input
-                        type="file"
-                        onChange={(e) => {
-                            const file = e.target.files[0];
-                            readExcel(file);
-                        }}
-                    />
-                Choose File
-                </label>
-                <table>
-                    <tbody>
-                        
-                        {
-                            items.map((d) => (
-                                <tr key={d.Item}>
-                                    <td>{ExcelDateToJSDate(d.Date)}</td>
-                                </tr>
-                            ))
+    const convertToJson = (headers, data) => {
+        const rows = []
+        data.forEach(row => {
+        let rowData = {}
+        row.forEach((element, index) => {
+            rowData[headers[index]] = element
+        })
+        rows.push(rowData)
 
-                        }
-                        
-                    </tbody>
-                </table>
+        });
+        return rows
+    }
+
+    const importExcel = (e) => {
+        const file = e.target.files[0]
+
+        const reader = new FileReader()
+        reader.onload = (event) => {
+        //parse data
+
+        const bstr = event.target.result
+        const workBook = XLSX.read(bstr, { type: "binary" })
+
+        //get first sheet
+        const workSheetName = workBook.SheetNames[0]
+        const workSheet = workBook.Sheets[workSheetName]
+        //convert to array
+        const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 })
+        // console.log(fileData)
+        const headers = fileData[0]
+        const heads = headers.map(head => ({ title: head, field: head }))
+        setColDefs(heads)
+
+        //removing header
+        fileData.splice(0, 1)
+
+
+        setData(convertToJson(headers, fileData))
+    }
+
+    if (file) {
+        if (getExention(file)) {
+            reader.readAsBinaryString(file)
+        }
+        else {
+            alert("Invalid file input, Select Excel, CSV file")
+        }
+        } else {
+            setData([])
+            setColDefs([])
+        }
+  }
+
+  return (
+        <>
+            <div className="import">
+                <h4 align='center'>Import Data from Excel and from CSV</h4>
+                <input type="file" onChange={importExcel} />
+                <MaterialTable 
+                    title="Portfolio Data" 
+                    options={{
+                        search: false,
+                        showTitle: false,
+                        toolbar:false,
+                        paging: false,
+                    }}
+                    data={data} 
+                    columns={colDefs} 
+                />
             </div>
-            
         </>
-    );
+  );
 }
 
 export default File;
