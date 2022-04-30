5135d52588d65d9b1c72668f88047a0fd4d5592d
Hugo Paré <71098494+Hugo-Pare@users.noreply.github.com>
2022-04-30 14:25:33 -0400

    news api working

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { Component, useState } from 'react'
 import './Feed.css'
@@ -5,13 +6,38 @@ import HomeBox from './HomeBox';
 
 class Feed extends Component{
 
+    constructor(props) {
+        super(props)
+        this.state={
+            text: ''
+        }
+    }
+
+    async componentDidMount() {
+        var requestOptions = {
+            method: 'GET'
+        };
+
+        fetch("https://api.marketaux.com/v1/news/all?symbols=AAPL,TSLA&filter_entities=true&api_token=DoixORqY2kR8bCCiERMVK6Z844d6TNWXJUqY7oFo", requestOptions)
+            .then((response) => {
+                console.log(response);
+                response.json().then((data) => {
+                    console.log(data);
+                });
+            });
+    }
+
     render(){
+
         return(
             <>
                 <div className='text-padding'>
                     <HomeBox subject="Box 1"/>
                     <HomeBox subject="Box 2"/>
                     <HomeBox subject="Box 3"/>
+                </div>
+                <div>
+                    
                 </div>
             </>
         )
